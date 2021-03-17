2d4c45cce0f4a7aba1c658c51e234b81458f7e33
MateuxLucax <mateuxlucax@gmail.com>
2021-03-16 22:42:55 -0300

    feat: latest project bottom rocket

--- src/components/pages/Home/LatestProjects.tsx
+++ src/components/pages/Home/LatestProjects.tsx
@@ -1,10 +1,10 @@
-import { LatestProjectsSection } from "@/styles/pages/home/LatestProjects";
 import Link from 'next/link';
 import Image from 'next/image';
 import { Subtitle } from "@/styles/Typography";
 import { PrimaryButton } from "@/styles/Buttons";
 import { ContentList } from "@/styles/pages/home/Home";
 import HomeListItem from "@/models/HomeListItem";
+import { LatestProjectsSection, RocketsContainer, FirstPlume, SecondPlume, ThirdPlume } from "@/styles/pages/home/LatestProjects";
 
 interface HomeLatestProjectsProps {
   title: string;
@@ -38,6 +38,24 @@ export default function HomeLatestProjects({ title, buttonText, projects }: Home
         )}
       </ContentList>
       <PrimaryButton>{buttonText}</PrimaryButton>
+      <RocketsContainer>
+        <svg width="32" height="48" viewBox="0 0 32 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0 42.2614L16 0L32 42.2614L16 25.093L0 42.2614Z" fill="#3C308C" />
+          <path d="M0 42.2614L16 25.093L32 42.2614L16 31.6954L0 42.2614Z" fill="#241874" />
+          <path d="M10.6667 36.9784L16.0005 34.3369L21.3343 36.9784L16.0005 46.2223L10.6667 36.9784Z" fill="url(#paint0_linear)" />
+          <defs>
+            <linearGradient id="paint0_linear" x1="15.9996" y1="34.3386" x2="16" y2="46.2235" gradientUnits="userSpaceOnUse">
+              <stop stop-color="#FFA200" />
+              <stop offset="1" stop-color="#FFCC66" />
+              <stop offset="1" stop-color="#FFCC66" />
+            </linearGradient>
+          </defs>
+        </svg>
+
+        <ThirdPlume />
+        <SecondPlume />
+        <FirstPlume />
+      </RocketsContainer>
     </LatestProjectsSection>
   )
 }
--- src/styles/pages/home/LatestProjects.ts
+++ src/styles/pages/home/LatestProjects.ts
@@ -1,11 +1,69 @@
+import colors from '@/styles/theme/colors';
 import theme from '@/styles/theme/theme';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 export const LatestProjectsSection = styled.section`
   text-align: center;
-  padding-bottom: ${theme.space.lg};
 
   h2 {
     padding-top: ${theme.space.lg};
   }
+`
+
+const rocketAnimation = keyframes`
+  from {
+    transform: translateY(0rem);
+  }
+  to {
+    transform: translateY(-1rem);
+  }
+`
+
+export const RocketsContainer = styled.div`
+  padding-top: ${theme.space.xl};
+  z-index: 1;
+  margin-top: auto;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-bottom: ${theme.space.xl};
+
+  svg {
+    padding-bottom: ${theme.space.xxl};
+    animation: ${rocketAnimation} infinite 1.5s ease-in-out alternate;
+  }
+`
+
+export const FirstPlume = styled.div`
+  z-index: 1000;
+  bottom: 0;
+  height: 3rem;
+  position: absolute;
+  background: ${colors.primary};
+  backdrop-filter: blur(1rem);
+  width: 100%;
+  clip-path: polygon(50% 0, 0 100%, 100% 100%);
+`
+
+export const SecondPlume = styled.div`
+  z-index: 100;
+  bottom: 0;
+  height: 5rem;
+  position: absolute;
+  background: rgba(79, 69, 145, 0.5);
+  backdrop-filter: blur(1rem);
+  width: 70%;
+  clip-path: polygon(50% 0, 0 100%, 100% 100%);
+`
+
+export const ThirdPlume = styled.div`
+  position: absolute;
+  z-index: 100;
+  bottom: 0;
+  height: 7rem;
+  width: 25%;
+  background: rgba(123, 115, 175, 0.5);
+  backdrop-filter: blur(1rem);
+  clip-path: polygon(50% 0, 0 100%, 100% 100%); 
 `
